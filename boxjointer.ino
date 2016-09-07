03e66043bcf2504a1e8dddaa5bd141780cfb2f4a
Nils Eckert <nils.eckert@eckert-partner.it>
2016-09-07 12:36:23 +0200

    Buttons auf Startseite sichtbar

--- boxjointer.ino
+++ boxjointer.ino
@@ -1,32 +1,44 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+//#include <Adafruit_TFTLCD.h> // Hardware-specific library
+//Adafruit_TFTLCD tft(A3, A2, A1, A0, A4);
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;       // hard-wired for UNO shields anyway.
+#include <TouchScreen.h>
 
-#include <SPFD5408_Adafruit_GFX.h>    // Core graphics library
-#include <SPFD5408_Adafruit_TFTLCD.h> // Hardware-specific library
-#include <SPFD5408_TouchScreen.h>     // Touch library
+// most mcufriend shields use these pins and Portrait mode:
+uint8_t YP = A1;  // must be an analog pin, use "An" notation!
+uint8_t XM = A2;  // must be an analog pin, use "An" notation!
+uint8_t YM = 7;   // can be a digital pin
+uint8_t XP = 6;   // can be a digital pin
+uint8_t SwapXY = 0;
 
-// Calibrates value
-#define SENSIBILITY 300
-#define MINPRESSURE 10
+
+uint16_t TS_LEFT = 920;
+uint16_t TS_RT  = 150;
+uint16_t TS_TOP = 940;
+uint16_t TS_BOT = 120;
+char *name = "Unknown controller";
+
+// For better pressure precision, we need to know the resistance
+// between X+ and X- Use any multimeter to read it
+// For the one we're using, its 300 ohms across the X plate
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+TSPoint tp;
+
+#define MINPRESSURE 20
 #define MAXPRESSURE 1000
 
+#define SWAP(a, b) {uint16_t tmp = a; a = b; b = tmp;}
 
-//These are the pins for the shield!
-#define YP A1 
-#define XM A2 
-#define YM 7  
-#define XP 6 
+int16_t BOXSIZE;
+int16_t PENRADIUS = 3;
+uint16_t identifier, oldcolor, currentcolor;
+uint8_t Orientation = 1;    //PORTRAIT
 
-short TS_MINX=150;
-short TS_MINY=120;
-short TS_MAXX=920;
-short TS_MAXY=940;
-
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, SENSIBILITY);
-
-#define LCD_CS A3
-#define LCD_CD A2
-#define LCD_WR A1
-#define LCD_RD A0
-#define LCD_RESET A4 // Optional : otherwise connect to Arduino's reset pin
+int woodWidth = 2000;
+int bladeWidth = 280;
+int cutWidth = 500;
+int glueWidth = 10;
 
 // Assign human-readable names to some common 16-bit color values:
 #define BLACK   0x0000
@@ -38,31 +50,111 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, SENSIBILITY);
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
-
-uint16_t width = 0;
-uint16_t height = 0;
-
-
 void setup() {
-  tft.reset();
-  
-  tft.begin(0x9325);
+  TS_LEFT = 893; TS_RT = 145; TS_TOP = 930; TS_BOT = 135;
+  SwapXY = 1;
 
-  tft.setRotation(1); // Need for the Mega, please changed for your choice or rotation initial
+  Serial.begin(9600);
+  ts = TouchScreen(XP, YP, XM, YM, 300);
+  tft.begin(0x0);
+  tft.setRotation(Orientation);
+  tft.setTextColor(BLACK);
+  showWelcome();
+  showOverview();
+}
 
-  width = tft.width() - 1;
-  height = tft.height() - 1;
-
-  tft.setCursor (55, 50);
-  tft.setTextSize (3);
-  tft.setTextColor(RED);
+void clearScreen() {
   tft.fillScreen(WHITE);
-  tft.println("Hallo Welt!");
+}
+
+void showWelcome() {
+    clearScreen();
   
+    printText(F("BoxJointer"), 5, 5, 5);
+
+    printText(F("Release: Beta 1"), 5, 80, 2);
+
+    printText(F("Starting"), 5,200, 1);
+    
+    for (int i = 0; i < 3; ++i) {
+      tft.print(F("."));
+      delay(200);
+    }
+}
+
+void setupText(int x, int y, int size) {
+  tft.setCursor(x,y);
+  tft.setTextSize(size);
+}
+
+void printText(int intToPrint, int x, int y, int size) {
+  setupText(x,y,size);
+  tft.print(intToPrint);
+}
+
+void printText(const __FlashStringHelper * text, int x, int y, int size) {
+  setupText(x,y,size);
+  tft.print(text);
+}
+
+void showOverview() {
+  clearScreen();
+ 
+  printText(F("Einstellungen"), 5, 5, 3);
+
+  int secondColX = 200;
+  int curRow = 50;
+  int rowHeight = 20;
+  
+  printText(F("Blattbreite"), 5, curRow, 2);
+  printText(bladeWidth, secondColX,curRow,2);
+  curRow += rowHeight;
+  
+  printText(F("Schnittbreite"), 5, curRow, 2);
+  printText(cutWidth, secondColX,curRow,2);
+  curRow += rowHeight;
+
+  printText(F("Leimzugabe"), 5, curRow, 2);
+  printText(glueWidth, secondColX,curRow,2);
+  curRow += rowHeight;
+  
+  printText(F("Holzbreite"), 5, curRow, 2);
+  printText(woodWidth, secondColX,curRow,2);
+
+  Adafruit_GFX_Button buttons[2];
+
+  int buttonsY = 210; 
+
+  buttons[0].initButton(&tft, 80, buttonsY, 150, 50, BLACK, RED, WHITE, "Anpassen", 2);
+  buttons[1].initButton(&tft, 240, buttonsY, 150, 50, BLACK, RED, WHITE, "Start", 2);
+
+  buttons[0].drawButton();
+  buttons[1].drawButton();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  waitForTouch();
+  tft.print(F("."));
+  delay(200);
 }
+
+void waitForTouch() {
+  // a point object holds x y and z coordinates
+  
+  while(1) {
+    TSPoint p = ts.getPoint();
+
+    pinMode(XM, OUTPUT);
+    pinMode(YP, OUTPUT);
+    pinMode(XP, OUTPUT);
+    pinMode(YM, OUTPUT);
+    
+    if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+       Serial.print("X = "); Serial.print(p.x);
+       Serial.print("\tY = "); Serial.print(p.y);
+       Serial.print("\tPressure = "); Serial.println(p.z);
+       break;
+    }
+  }
+}
+
